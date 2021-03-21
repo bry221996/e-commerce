93fca25db1e87c21900f8563c6c9031600ab9964
Bryan Mulingbayan <bryan.mulingbayan96@gmail.com>
2021-03-21 20:05:31 +0800

    categories migration file

---
+++ database/migrations/2021_03_21_083149_create_categories_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('store_id');
+            $table->string('title');
+            $table->string('description');
+            $table->boolean('is_active')->default(0);
+            $table->uuid('parent_id')->nullable();
+            $table->timestamps();
+
+            $table->foreign('store_id')
+                ->references('id')
+                ->on('stores');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
