11663bdc32290c6c7b32354017b2c64b1860a0ac
Bryan Mulingbayan <bryan.mulingbayan96@gmail.com>
2021-03-21 16:25:24 +0800

    update store id data type

--- app/Models/Store.php
+++ app/Models/Store.php
@@ -4,12 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Store extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    /**
+     * The "booted" method of the model.
+     *
+     * @return void
+     */
+    protected static function booted()
+    {
+        static::creating(function ($store) {
+            $store->id = Str::uuid()->toString();
+        });
+    }
 
     public function getRouteKeyName()
     {
--- database/migrations/2021_02_28_135544_create_stores_table.php
+++ database/migrations/2021_02_28_135544_create_stores_table.php
@@ -14,7 +14,7 @@ class CreateStoresTable extends Migration
     public function up()
     {
         Schema::create('stores', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id');
             $table->unsignedBigInteger('owner_id');
             $table->string('name');
             $table->string('identifier');
