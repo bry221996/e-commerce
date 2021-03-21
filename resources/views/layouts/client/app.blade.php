c3a375c97673d921477ef26afbaf18d3f8fc8b22
Bryan Mulingbayan <bryan.mulingbayan96@gmail.com>
2021-03-21 19:18:25 +0800

    update view

--- resources/views/layouts/client/app.blade.php
+++ resources/views/layouts/client/app.blade.php
@@ -6,7 +6,7 @@
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ $store->name }}</title>
 
     <!-- General CSS Files -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
