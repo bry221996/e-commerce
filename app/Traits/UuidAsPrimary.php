15e6913486c33153f7953a0c44ce2c988292b84e
Bryan Mulingbayan <bryan.mulingbayan96@gmail.com>
2021-03-22 11:03:46 +0800

    Add custom traits

---
+++ app/Traits/UuidAsPrimary.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Support\Str;
+
+trait UuidAsPrimary
+{
+    /**
+     * The "booted" method of the model.
+     *
+     * @return void
+     */
+    protected static function booted()
+    {
+        static::creating(function ($model) {
+            $model->id = Str::uuid()->toString();
+        });
+    }
+
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    public function getKeyType()
+    {
+        return 'string';
+    }
+}
