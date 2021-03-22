49d80051fb333cf651e670993f258e082453dc93
Bryan Mulingbayan <bryan.mulingbayan96@gmail.com>
2021-03-22 11:04:58 +0800

    update routes

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::domain('{store}.localhost')->group(function () {
     Route::group(['prefix' => 'admin'], function () {
         Route::get('dashboard', [StoreController::class, 'index']);
-        Route::get('categories', [CategoryController::class, 'index']);
+        Route::resource('categories', CategoryController::class);
     });
 });
 
